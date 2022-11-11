871f86004df84f6c517fe5b809e1ba8d9d1e1183
tolentinoo <rtolentino@smith.edu>
2022-11-10 21:56:08 -0500

    sum

--- Tamagotchi.java
+++ Tamagotchi.java
@@ -7,7 +7,7 @@ public class Tamagotchi { // implements Contract
     public String color; 
     private int age;
     private int locationX;
-    priavte int locationY;
+    private int locationY;
     private ArrayList <String> inventory;
 
 
@@ -17,19 +17,19 @@ public class Tamagotchi { // implements Contract
         this.name = name;
         this.color = color; 
         this.age = 1;
-        this.locationX =1 ;
-        this.locationY;
+        this.locationX = 0 ;
+        this.locationY= 0 ;
     }
 
     // Accessors & Manipulators 
-
+    
 
     // Methods 
     public void grab(String item){
 
     }
     public String drop(String item){
-
+         
     }
     public void examine(String item){
 
@@ -37,22 +37,22 @@ public class Tamagotchi { // implements Contract
     public void use(String item){
 
     }
-    public boolean walk(String direction){
+    // public boolean walk(String direction){
 
-    }
-    public boolean fly(int x, int y){
+    // }
+    // public boolean fly(int x, int y){
 
-    }
-    public Number shrink(){
+    // }
+    // public Number shrink(){
 
-    }
-    public Number grow(){
+    // }
+    // public Number grow(){
 
-    }
-    public void rest(){
+    // }
+    // public void rest(){
 
-    }
-    public void undo(){
+    // }
+    // public void undo(){
 
     }
 
