533004b2c81c184d26dc30d021ff372a0d8fbaf7
tolentinoo <rtolentino@smith.edu>
2022-11-15 22:14:42 -0500

    Done

--- Tamagotchi.java
+++ Tamagotchi.java
@@ -9,7 +9,8 @@ public class Tamagotchi { // implements Contract
     private int locationX;
     private int locationY;
     private ArrayList <String> inventory;
-
+    private ArrayList <String> powerUp;
+    
 
 
     // Constrtuctor 
@@ -22,33 +23,66 @@ public class Tamagotchi { // implements Contract
     }
 
     // Accessors & Manipulators 
-    
+    public void setName(String name2) {
+        this.name = name2;
+    }
 
     // Methods 
     public void grab(String item){
-
+        System.out.println("You have grabbed" + item +" it is being added to your inventory");
+        inventory.add(item);
     }
+
     public String drop(String item){
-         
+         inventory.remove(item);
     }
+
     public void examine(String item){
-
+        if (powerUp.contains(item) == true ){
+            grab(item);
+        }
+        else{
+            drop(item);
+        }
     }
+
     public void use(String item){
-
+        if (inventory.contains(item) == true);
+            System.out.println("You have unlocked a new achievement ;) Do you want to use it?");
     }
-    // public boolean walk(String direction){
 
-    // }
+    public boolean walk(String direction){
+        direction.toLowerCase();
+        if (direction == "north"){
+            locationY +=1;   
+        }
+        if (direction == "south"){
+            locationY -=1;
+        } 
+        if (direction == "west"){
+            locationX -=1;
+        }
+        if (direction == "east"){
+            locationX += 1;
+        }
+        else{
+            System.out.println("Give a valid direction");
+        }
+    }
+
     // public boolean fly(int x, int y){
-
+    //     if (fly )
+    //     locationX += x;
+    //     locationY += y;
     // }
-    // public Number shrink(){
 
-    // }
-    // public Number grow(){
+    public Number shrink(){
+        this.age -= 10;
+    }
 
-    // }
+    public Number grow(){
+        this.age += 10;
+    }
     // public void rest(){
 
     // }
